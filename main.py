3f15e436c17adebedb8560e7852338e5fad0876f
thrain-traitor <arkadanisaysstuff@gmail.com>
2025-03-18 00:53:00 +0700

    update

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
+import cv2
 import numpy as np
 
-a = input("angka kesatu")
-b = input("angka kedua")
+cap = cv2.VideoCapture(0)
 
-nilai = [76,50.80,97,40,60]
+ret, frame1 = cap.read()
+ret, frame2 = cap.read()
 
-c = int(a) + int(b)
+while cap.isOpened():
+    diff = cv2.absdiff(frame1, frame2)
+    
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    
+    dilated = cv2.dilate(thresh, None, iterations=3)
 
-if c > np.mean(nilai):
-  print(c, "anda lulus nilai rata rata nya adalah ", np.mean(nilai))
-else :
-  print(c, "anda gagal nilai rata rata nya adalah ", np.mean(nilai))
+    contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 500:
+            continue 
+            
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imwrite("gerakan_terdeteksi.jpg", frame1)
+        print("Gambar disimpan!")
+
+    cv2.imshow("Sensor Gerakan", frame1)
+
+    frame1 = frame2
+    ret, frame2 = cap.read()
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
